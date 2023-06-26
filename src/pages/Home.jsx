daa5dfd00d4bc8116b6bcd88d97cd32a654ecad9
connorwarme <connor.warme@gmail.com>
2023-06-26 13:59:11 -0600

    Refresh on simple event handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('handle click event', e.target)
+  }
+  const handleWithArg = (input) => {
+    console.log('handle click with ' + input)
+  }
   return ( 
     <>
       <div className="home-container">
         <div className="home-content">
           <h1 className="title">Rings of Power Fan Lair</h1>
+          <button onClick={handleClick} >Click Me!</button>
+          <button onClick={() => handleWithArg('value')}>Click to add value!</button>
         </div>
       </div>
     </>

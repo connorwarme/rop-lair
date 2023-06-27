a819c39f63ea2a15b91d13047163fe0c89184860
connorwarme <connor.warme@gmail.com>
2023-06-26 22:32:05 -0600

    Implement useEffect - in progress, bug w/ my API?

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PostList from '../components/PostList';
 
 const Profile = () => {
@@ -15,6 +15,34 @@ const Profile = () => {
     {title: "Post 3", author: "Caleb", description: "A day at the lake...", id: 3},
   ])
 
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3000/profile', {
+      method: "GET",
+      mode: "no-cors",
+      headers: {
+        "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0OTVkYTZkNWRlYTgwZmM2NWEwYTQ0NyIsImZpcnN0X25hbWUiOiJjb24iLCJmYW1pbHlfbmFtZSI6Im1hbiIsImVtYWlsIjoiY0BnbWFpbC5jb20iLCJsb2dpbmlkIjp7Imhhc2giOiIkMmEkMTIkbndCazFJUVpsU1BOOFVHRXFWcHl5ZUdmNFdwMmF1VGFHWm5nbGlFT1RuVnZMbmNvWXdqTXkiLCJnb29nbGVpZCI6bnVsbCwiZmJpZCI6bnVsbH0sImZyaWVuZF9saXN0IjoiNjQ5NWRhNmQ1ZGVhODBmYzY1YTBhNDQ2IiwiX192IjowfSwiaWF0IjoxNjg3NTQzMjk2fQ.BmCtxFg9iTT2PU1AwG-q_Gq2DSqgVFNmAydD47wTMrk',
+      }
+    })
+    .then((res) => {
+      console.log(res)
+      if (!res.ok) {
+        throw Error('Problem getting a response from the server.')
+        
+      }
+      return res.json()
+    })
+    .then((data) => {
+      console.log(data)
+      setError(null)
+    })
+    .catch((err) => {
+      console.log(err.message)
+      setError(err.message)
+    })
+  })
+
   const handleClick = () => {
     const userCopy = { ...user }
     userCopy.name = 'Amity Warme',
@@ -25,6 +53,7 @@ const Profile = () => {
     <>
       <div className="profile-container">
         <div className="profile-content">
+          { error && <div>{error}</div> }
           <h1 className="profile-title">{user.name}</h1>
           <p className="description">{user.description}</p>
           <button onClick={handleClick}>Click me!</button>

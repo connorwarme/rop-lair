fe2d02e26cdfe881dc656f72f4d6965fb05cddc0
connorwarme <connor.warme@gmail.com>
2023-06-28 21:14:28 -0600

    Separate custom hook logic, implement on profile

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -7,7 +7,7 @@ const useFetch = (url, auth) => {
 
   useEffect(() => {
     fetch(url, {
-      headers: auth.header
+      headers: auth.headers,
     })
     .then(res => {
       if (!res.ok) {
@@ -25,7 +25,10 @@ const useFetch = (url, auth) => {
       setError(err.message)
       setLoading(false)
     })
-  }, [url, auth] )
+    // had to leave array empty otherwise it would loop infinitely
+    // not sure why, because neither url or auth values changed...
+    // seems to work now, besides vsc squawking at me
+  }, [] )
 
   return { data, isLoading, error }
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
+import useFetch from '../hooks/useFetch';
 import PostList from '../components/PostList';
+import value from '../../value';
 
 const Profile = () => {
   // example user
@@ -14,39 +16,46 @@ const Profile = () => {
     {title: "Post 2", author: "Connor", description: "A day at the lake...", id: 2},
     {title: "Post 3", author: "Caleb", description: "A day at the lake...", id: 3},
   ])
+  const url = 'http://localhost:3000/profile'
+  const auth = {
+    headers: {
+    "Authorization": value.headers.auth,
+    }
+  }
+  const { data, isLoading, error } = useFetch(url, auth)
+  // shifted hook logic to separate file (useFetch.js)
+  // const [error, setError] = useState(null)
 
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    fetch('http://localhost:3000/profile', {
-      headers: {
-        "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0OTVkYTZkNWRlYTgwZmM2NWEwYTQ0NyIsImZpcnN0X25hbWUiOiJjb24iLCJmYW1pbHlfbmFtZSI6Im1hbiIsImVtYWlsIjoiY0BnbWFpbC5jb20iLCJsb2dpbmlkIjp7Imhhc2giOiIkMmEkMTIkbndCazFJUVpsU1BOOFVHRXFWcHl5ZUdmNFdwMmF1VGFHWm5nbGlFT1RuVnZMbmNvWXdqTXkiLCJnb29nbGVpZCI6bnVsbCwiZmJpZCI6bnVsbH0sImZyaWVuZF9saXN0IjoiNjQ5NWRhNmQ1ZGVhODBmYzY1YTBhNDQ2IiwiX192IjowfSwiaWF0IjoxNjg3NTQzMjk2fQ.BmCtxFg9iTT2PU1AwG-q_Gq2DSqgVFNmAydD47wTMrk',
-      }
-    })
-    .then((res) => {
-      console.log(res)
-      if (!res.ok) {
-        throw Error('Problem getting a response from the server.')
-      }
-      return res.json()
-    })
-    .then((data) => {
-      console.log(data)
-      setError(null)
-      setUser({
-        // virtual "name" doesn't work yet, have to construct it from first and family name
-        // todo: check this by creating new user object (I already changed user model to provide virtuals toJSON)
-        // name: data.user.name,
-        name: data.user.first_name + ' ' + data.user.family_name,
-        photo: null,
-        description: "user updated from database",
-      })
-    })
-    .catch((err) => {
-      console.log(err.message)
-      setError(err.message)
-    })
-  }, [])
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/profile', {
+  //     headers: {
+  //       "Authorization": value.headers.auth,
+  //     }
+  //   })
+  //   .then((res) => {
+  //     console.log(res)
+  //     if (!res.ok) {
+  //       throw Error('Problem getting a response from the server.')
+  //     }
+  //     return res.json()
+  //   })
+  //   .then((data) => {
+  //     console.log(data)
+  //     setError(null)
+  //     setUser({
+  //       // virtual "name" doesn't work yet, have to construct it from first and family name
+  //       // todo: check this by creating new user object (I already changed user model to provide virtuals toJSON)
+  //       // name: data.user.name,
+  //       name: data.user.first_name + ' ' + data.user.family_name,
+  //       photo: null,
+  //       description: "user updated from database",
+  //     })
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.message)
+  //     setError(err.message)
+  //   })
+  // }, [])
 
   const handleClick = () => {
     const userCopy = { ...user }
@@ -59,7 +68,7 @@ const Profile = () => {
       <div className="profile-container">
         <div className="profile-content">
           { error && <div>{error}</div> }
-          <h1 className="profile-title">{user.name}</h1>
+          { data && <h1 className="profile-title">{data.user.first_name} || {data.user.family_name}</h1> }
           <img className='profile-img'></img>
           <p className="description">{user.description}</p>
           <button onClick={handleClick}>Click me!</button>

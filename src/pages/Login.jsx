fafbebcbb6d9ec598fc8b6c49f4bdb05938de907
connorwarme <connor.warme@gmail.com>
2023-07-01 22:01:56 -0600

    Notes for next time...still debugging

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,18 +16,26 @@ const Login = () => {
 
   // removed headers and it goes through, but gives error - user not found (in db).
   // not sure why?? is a user currently logged in? need to debug...
+  const getData = () => {
+    console.log('get data fired!')
+    return { email: email, password: password }
+  }
   const handleSubmit = (e) => {
+    // okay, not sure what is going on.
+    // I click submit and the form submits. 
+    // the initial passport function runs but nothing else in the process...
+    // current theory is that I'm failing to pass along the user inputs in the request body
+    // but I haven't been able to confirm exactly how to do that / confirm that the data is going through.
+    // other theory was that I passed through empty data values
+    // I could check if the original state values are what is getting passed along, which would indicate a problem with my react code...
+    const data = getData()
     e.preventDefault()
-
     fetch(url, {
       method: 'POST',
-      // headers: {
-      //   "Content-Type": "application/json"
-      //   },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      })
+      headers: {
+        "Content-Type": "application/json"
+        },
+      body: JSON.stringify(data)
     })
     .then(res => {
       if (!res.ok) {

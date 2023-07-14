02b1ce3c99d19e8aa62033c9e63ab2314c12f34c
connorwarme <connor.warme@gmail.com>
2023-07-13 18:57:28 -0600

    Add useEffect cleanup (abort on onmount)

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,12 +2,16 @@ import { useState, useEffect } from "react"
 
 const useFetch = (url, auth) => {
   const [data, setData] = useState(null)
+  // tutorial example has a setTimeout (1s) delay on the fetch and shows a loading screen momentarily... (so it doesn't look like the webpage is broken)
   const [isLoading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
   useEffect(() => {
+    const abortController = new AbortController()
+
     fetch(url, {
       headers: auth.headers,
+      signal: abortController.signal,
     })
     .then(res => {
       if (!res.ok) {
@@ -24,9 +28,15 @@ const useFetch = (url, auth) => {
     // todo: check this by creating new user object (I already changed user model to provide virtuals toJSON)
     })
     .catch(err => {
-      setError(err.message)
-      setLoading(false)
+      if (err.name === 'AbortError') {
+        console.log('fetch aborted (component unmounted before completed)')
+      } else {
+        setError(err.message)
+        setLoading(false)
+      }
     })
+    return () => abortController.abort()
+    
     // had to leave array empty otherwise it would loop infinitely
     // not sure why, because neither url or auth values changed...
     // seems to work now, besides vsc squawking at me

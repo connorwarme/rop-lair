e529306092c502a9ab3e604c7fea4342dc2b830f
connorwarme <connor.warme@gmail.com>
2023-07-22 12:28:10 -0600

    Organize nav, simple styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,15 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom"
 import axios from "axios";
 import { clearStorage, returnObject } from "../utility/ls";
+import { myContext } from "../contexts/Context";
+import "../styles/navStyle.css"
 
 // want to think through how to hide routes when the visitor has not signed in yet
 // want to show routes once signed in ... but hide the sign in and login links
 const Nav = () => {
+
+  const { userObject } = useContext(myContext)
 
   // need to send auth bearer token and refresh token in request
   // then need to remove tokens from local storage
@@ -31,12 +36,22 @@ const Nav = () => {
   return ( 
     <>
       <div className="nav-container">
+        { userObject &&
         <div className="nav-links">
           <Link to="/" className="home">Home</Link>
           <Link to="/profile" className="profile">Profile</Link>
           <Link to="/users" className="users">Users</Link>
+          <Link to="/profile" className="user">{userObject.name}</Link>
           <li className="logout" onClick={logout}>Logout</li>
         </div>
+        }
+        {
+          userObject === null && 
+          <div className="nav-links">
+            <Link className="login">Login</Link>
+            <Link className="signup">Sign-Up</Link>
+          </div>
+        }
       </div>
     </>
    );

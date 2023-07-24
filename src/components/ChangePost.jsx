c3a96375219b95378188c365a437304985640c1f
connorwarme <connor.warme@gmail.com>
2023-07-24 17:24:47 -0600

    Create changePost so create and edit can use it

---
+++ src/components/ChangePost.jsx
@@ -0,0 +1,81 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { myContext } from "../contexts/Context";
+
+const ChangePost = ({ url, post }) => {
+  // this could be a new post or an edit post...
+  // differences?
+  // cancel edit would redirect to post detail page / but cancel create would redirect to home
+  const [title, setTitle] = useState( post ? post.title : '')
+  const [content, setContent] = useState( post ? post.content : '')
+  const [errors, setErrors] = useState(null)
+
+  const { access } = useContext(myContext)
+  const navigate = useNavigate()
+
+  const handleChange = (e, updateFn) => {
+    updateFn(e.target.value)
+  }
+  const getState = () => {
+    return { title, content }
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const post = getState()
+    // reset form?
+    // or don't allow submit button to get clicked again...
+
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${access}`
+    }
+    console.log(access)
+    axios.post(url, post, { headers: headers })
+    .then(res => {
+      console.log(res)
+      if (res.status === 200 && res.data.post) {
+        // need to set up post detail page to receive / use state
+        navigate(`/post/${res.data.post._id}`, { state: res.data })
+      }
+      if (res.status === 200 && res.data.errors) {
+        // display errors on form 
+        // provide their data (so they don't have to retype everything)
+        setErrors(res.data.errors)
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  return ( 
+    <>
+      <div className="change-post-container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-input">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" className="title" value={title} onChange={(e) => {handleChange(e, setTitle)}}/>
+          </div>
+          <div className="form-input">
+            <label htmlFor="content">Content</label>
+            <input type="text" id="content" className="content" value={content} onChange={(e) => {handleChange(e, setContent)}}/>
+          </div>
+          <button type="submit">Save</button>
+          { errors && (
+            <div className="errors">
+              { errors.map((err, index) => {
+                if (err.status) {
+                  return <p key={index}>{err.status} Error! {err.msg}</p>
+                }
+                return <p key={index}>Error! {err.msg}</p>
+              })}
+            </div>
+          )}
+        </form>
+      </div>
+    </>
+   );
+}
+ 
+export default ChangePost;
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,79 +1,89 @@
-import { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { myContext } from "../contexts/Context";
+// import { useState, useContext } from "react";
+// import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { myContext } from "../contexts/Context";
+
+// const CreatePost = () => {
+//   const [title, setTitle] = useState(null)
+//   const [content, setContent] = useState(null)
+//   const [errors, setErrors] = useState(null)
+//   // need to import context
+//   const { access } = useContext(myContext)
+//   const navigate = useNavigate()
+
+//   const handleChange = (e, updateStateFn) => {
+//     updateStateFn(e.target.value)
+//   }
+//   const getState = () => {
+//     return { title: title, content: content }
+//   }
+//   const handleSubmit = (e) => {
+//     e.preventDefault()
+//     const post = getState()
+//     // reset form?
+//     // or don't allow submit button to get clicked again...
+
+//     const headers = {
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${access}`
+//     }
+//     console.log(access)
+//     axios.post("http://localhost:3000/createpost", post, { headers: headers })
+//     .then(res => {
+//       console.log(res)
+//       if (res.status === 200 && res.data.post) {
+//         // need to set up post detail page to receive / use state
+//         navigate(`/post/${res.data.post._id}`, { state: res.data })
+//       }
+//       if (res.status === 200 && res.data.errors) {
+//         // display errors on form 
+//         // provide their data (so they don't have to retype everything)
+//         setErrors(res.data.errors)
+//       }
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+//   }
+
+//   return ( 
+//     <>
+//       <div className="create-container">
+//         <form className="create-post" onSubmit={handleSubmit}>
+//           <div className="form-input">
+//             <label htmlFor="title">Title</label>
+//             <input type="text" id="title" className="title" onChange={(e) => {handleChange(e, setTitle)}}/>
+//           </div>
+//           <div className="form-input">
+//             <label htmlFor="content">Content</label>
+//             <input type="text" id="content" className="content" onChange={(e) => {handleChange(e, setContent)}}/>
+//           </div>
+//           <button type="submit">Save</button>
+//           { errors && (
+//             <div className="errors">
+//               { errors.map((err, index) => {
+//                 if (err.status) {
+//                   return <p key={index}>{err.status} Error! {err.msg}</p>
+//                 }
+//                 return <p key={index}>Error! {err.msg}</p>
+//               })}
+//             </div>
+//           )}
+//         </form>
+
+//       </div>
+//     </>
+//    );
+// }
+
+import ChangePost from "../components/ChangePost";
 
 const CreatePost = () => {
-  const [title, setTitle] = useState(null)
-  const [content, setContent] = useState(null)
-  const [errors, setErrors] = useState(null)
-  // need to import context
-  const { access } = useContext(myContext)
-  const navigate = useNavigate()
-
-  const handleChange = (e, updateStateFn) => {
-    updateStateFn(e.target.value)
-  }
-  const getState = () => {
-    return { title: title, content: content }
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const post = getState()
-    // reset form?
-    // or don't allow submit button to get clicked again...
-
-    const headers = {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${access}`
-    }
-    console.log(access)
-    axios.post("http://localhost:3000/createpost", post, { headers: headers })
-    .then(res => {
-      console.log(res)
-      if (res.status === 200 && res.data.post) {
-        // need to set up post detail page to receive / use state
-        navigate(`/post/${res.data.post._id}`, { state: res.data })
-      }
-      if (res.status === 200 && res.data.errors) {
-        // display errors on form 
-        // provide their data (so they don't have to retype everything)
-        setErrors(res.data.errors)
-      }
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
-
-  return ( 
+  const url = "http://localhost:3000/createpost"
+  return (
     <>
-      <div className="create-container">
-        <form className="create-post" onSubmit={handleSubmit}>
-          <div className="form-input">
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" className="title" onChange={(e) => {handleChange(e, setTitle)}}/>
-          </div>
-          <div className="form-input">
-            <label htmlFor="content">Content</label>
-            <input type="text" id="content" className="content" onChange={(e) => {handleChange(e, setContent)}}/>
-          </div>
-          <button type="submit">Save</button>
-          { errors && (
-            <div className="errors">
-              { errors.map((err, index) => {
-                if (err.status) {
-                  return <p key={index}>{err.status} Error! {err.msg}</p>
-                }
-                return <p key={index}>Error! {err.msg}</p>
-              })}
-            </div>
-          )}
-        </form>
-
-      </div>
+      <ChangePost url={url} post={false} />
     </>
-   );
+  )
 }
- 
 export default CreatePost;

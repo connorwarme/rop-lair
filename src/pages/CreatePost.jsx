27436d91420b35f660f9358d0721eacb2791a01e
connorwarme <connor.warme@gmail.com>
2023-07-23 21:09:49 -0600

    CreatePost now creates a post! proper header.

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useContext } from "react";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { myContext } from "../contexts/Context";
 
 const CreatePost = () => {
   const [title, setTitle] = useState(null)
   const [content, setContent] = useState(null)
+  const [errors, setErrors] = useState(null)
   // need to import context
   const { access } = useContext(myContext)
+  const navigate = useNavigate()
 
   const handleChange = (e, updateStateFn) => {
     updateStateFn(e.target.value)
@@ -16,21 +19,27 @@ const CreatePost = () => {
   }
   const handleSubmit = (e) => {
     e.preventDefault()
-    // need to write this!
-    // for some reason I'm not getting my header attached to the request. my backend receives an undefined auth header. 
     const post = getState()
+    // reset form?
+    // or don't allow submit button to get clicked again...
+
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${access}`
+    }
     console.log(access)
-    axios.post("http://localhost:3000/createpost", {
-      headers: {
-        "Authorization": `Bearer ${access}`
-      }, 
-      data: {
-        title: post.title,
-        content: post.content,
-      }
-    })
+    axios.post("http://localhost:3000/createpost", post, { headers: headers })
     .then(res => {
       console.log(res)
+      if (res.status === 200 && res.data.post) {
+        // need to set up post detail page to receive / use state
+        navigate(`/post/${res.data.post._id}`, { state: res.data })
+      }
+      if (res.status === 200 && res.data.errors) {
+        // display errors on form 
+        // provide their data (so they don't have to retype everything)
+        setErrors(res.data.errors)
+      }
     })
     .catch(err => {
       console.log(err)
@@ -50,7 +59,9 @@ const CreatePost = () => {
             <input type="text" id="content" className="content" onChange={(e) => {handleChange(e, setContent)}}/>
           </div>
           <button type="submit">Save</button>
+          
         </form>
+
       </div>
     </>
    );

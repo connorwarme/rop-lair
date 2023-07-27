f5929796cd99462296be8d039a9bba3d2f340163
connorwarme <connor.warme@gmail.com>
2023-07-27 13:26:01 -0600

    Working on edit comment fnality

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -6,14 +6,18 @@ const Comment = ({ commentObj, user, makeHeader }) => {
   const [author, setAuthor] = useState(false)
   // is the current user the author of the comment?
   const [isAuthor, setIsAuthor] = useState(false)
-  // need an axios request to get comment author from db
+  const [comment, setComment] = useState(false)
+  const [edit, setEdit] = useState(false)
 
+
+  // need an axios request to get comment author from db
   useEffect(() => {
     axios.get("http://localhost:3000/author/" + commentObj.author, { headers: makeHeader() })
     .then(res => {
       if (res.status === 200 && res.data.author) {
         setAuthor(res.data.author)
         setIsAuthor(res.data.author._id === user._id)
+        setComment(commentObj.content)
       }
     })
     .catch(err => {
@@ -31,13 +35,44 @@ const Comment = ({ commentObj, user, makeHeader }) => {
   // edit and delete buttons if comment author is user
   // handle edit and handle delete functions
   // edit view (in place) w/ cancel and save buttons
+  const handleCommentEdit = (e) => {
+    setComment(e.target.value)
+  }
+  const handleCancelEdit = () => {
+    setEdit(false)
+    setComment(commentObj.content)
+  }
+  const handleSaveEdit = () => {
+    // axios post req to save 
+  }
+  const handleShowEdit = () => {
+    setEdit(true)
+  }
+  const handleDelete = () => {
+    // axios req to delete
+  }
   return ( 
     <>
       <div className="comment-container">
         <div className="comment-content">
-          { author && <h5 className="comment-author">{author.name}</h5> }
-          <p className="comment-text">{commentObj.content}</p>
-          { (isAuthor) && <button onClick={() => console.log('Delete this comment from db.')}>Delete</button>}
+          { !edit && (
+            <div className="comment-unit">
+              <h5 className="comment-author">{author ? author.name : 'Anonymous Author'}</h5>
+              <p className="comment-text">{commentObj.content}</p>
+            </div>
+          )}
+          { edit && (
+            <div className="comment-unit edit">
+              <form onSubmit={handleSaveEdit}>
+                <label htmlFor="comment">Comment:</label>
+                <input type="text" id='comment' value={comment} onChange={handleCommentEdit} />
+                <button type='button' onClick={handleCancelEdit}>Cancel</button>
+                <button type='submit'>Save</button>
+              </form>
+            </div>
+          )}
+          { (isAuthor && !edit) && <button onClick={handleShowEdit}>Edit</button> }
+          { (isAuthor) && <button onClick={handleDelete}>Delete</button> }
         </div>
       </div>
     </>

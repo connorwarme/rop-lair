5d4d24099a397b600e9f6b58abaa28d3fe9f0f75
connorwarme <connor.warme@gmail.com>
2023-07-29 21:53:35 -0600

    Begin thinking about add friend feature

---
+++ src/components/AddFriend.jsx
@@ -0,0 +1,20 @@
+const AddFriend = ({ user, profile }) => {
+  // my idea here is to have a little feature that provides the friendship status
+  // and the action: accept / reject, add friend, ..(revoke friend request?)
+  // trying to think through the logic
+  // use cases:
+  // when user visits another person's profile page
+  // when user visits another person's friend list
+  // when user visits a list of site members
+  
+
+  return ( 
+    <>
+      <div className="add-friend-container">
+
+      </div>
+    </>
+   );
+}
+ 
+export default AddFriend;

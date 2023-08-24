4850d200f09c7f062c5c790e9b69cdfa5cd1af10
connorwarme <connor.warme@gmail.com>
2023-08-24 16:52:39 -0600

    Continue building other profile page

--- src/pages/OtherProfile.jsx
+++ src/pages/OtherProfile.jsx
@@ -2,10 +2,12 @@ import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { myContext } from "../contexts/Context";
 import useFetch from "../hooks/useFetch";
+import Profile from "./Profile";
+import PostList from "../components/PostList";
 
 const OtherProfile = () => {
   const { id } = useParams()
-  const { access } = useContext(myContext)
+  const { userObject, access, setUserObject } = useContext(myContext)
 
   const url = 'http://localhost:3000/profile' + id
   const auth = {
@@ -14,13 +16,42 @@ const OtherProfile = () => {
     }
   }
   const { data, isLoading, error } = useFetch(url, auth)
-  // any other profile (besides the user)
-  // need to fetch data from backend
-
+  const setList = (array) => {
+    const newObj = {...userObject}
+    newObj.friend_list = array
+    setUserObject(newObj)
+  }
 
   return ( 
     <>
-
+      { (userObject && !isLoading) && (data && data.profile) && (
+        <>
+          <Profile userObject={userObject} profile={data.profile} setList={setList} />
+        </>
+      )}
+      { (userObject && !isLoading) && (
+        <>
+          { data && (
+            <>
+            { data.posts && (
+              <PostList posts={data.posts} full={true} user={userObject} />
+            )}
+            { data.errors && (
+              <div className="errors-container">
+                <h4>Error Loading Posts</h4>
+                <div>{data.errors[0].msg}</div> 
+              </div>
+            )}
+            </>
+          )}
+          { error && (
+            <div className="errors-container">
+              <h4>Error Loading Posts</h4>
+              <div>{error}</div>
+            </div>
+          )}
+        </>
+      )}
     </> 
   );
 }

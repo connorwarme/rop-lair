e4d7bf051c1ff0a92e5cfbf89a2862aaf1cb52d0
connorwarme <connor.warme@gmail.com>
2023-09-02 13:38:45 -0600

    Begin building users list page, needs testing

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,43 @@
+import { useContext } from "react";
+import { myContext } from "../contexts/Context";
+import useFetch from "../hooks/useFetch";
+import UserUnit from "../components/UserUnit";
 const Users = () => {
+
+  const { userObject, setUserObject, access } = useContext(myContext)
+  const profile = {
+    name: 'Conrad Wildman',
+    picture: 'https://avatars.githubusercontent.com/u/43254103?v=4',
+  }
+  const url = 'http://localhost:3000/users/'
+  const auth = {
+    headers: {
+      "Authorization": `Bearer ${access}`
+    }
+  }
+  const { data, isLoading, error} = useFetch(url, auth)
+
   return ( 
     <>
-      <h1>This is the User List page.</h1>
+      { isLoading && <h3>Page is loading...</h3> }
+      { !isLoading && !error && (
+        <>
+          <div className="user-list-container">
+            <h1>This is the User List page.</h1>
+            { (data && data.users) && (
+              data.users.map(user => {
+                return (
+                  <>
+                    <UserUnit userObject={userObject} profile={user} />
+                  </>
+                )
+              })
+            )}
+          </div>
+        </>
+      )}
     </>
-   );
+   )
 }
  
 export default Users;

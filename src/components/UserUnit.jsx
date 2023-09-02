966243273cea15ca179823223db962d95a6bce17
connorwarme <connor.warme@gmail.com>
2023-09-02 14:32:35 -0600

    Include link to user profile and addfriend comp

--- src/components/UserUnit.jsx
+++ src/components/UserUnit.jsx
@@ -1,13 +1,20 @@
+import { Link } from "react-router-dom";
+import OtherProfile from "../pages/OtherProfile";
 import AddFriend from "./AddFriend";
 import decodeEscapedData from "../utility/escape";
 
-const UserUnit = ({ userObject, profile }) => {
+const UserUnit = ({ userObject, profile, setList }) => {
   // need to add in addFriend component
   return ( 
     <>
       <div className="userUnit-container">
-        <img src={profile.picture} alt="User Icon" height={'80px'}/>
-        <h4>{decodeEscapedData(profile.name)}</h4>
+        <Link to={`/user/${profile._id}`} element={<OtherProfile />} >
+          <div className="userUnit-content">
+            <img src={profile.picture} alt="User Icon" height={'80px'}/>
+            <h4>{decodeEscapedData(profile.name)}</h4>
+          </div>
+        </Link>
+        <AddFriend list={userObject.friend_list} setList={setList} profileId={profile._id} />
       </div>
     </>
    );

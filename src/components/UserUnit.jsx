f9e0ecfd029ab6a6c61daea49b07d1360ab492d4
connorwarme <connor.warme@gmail.com>
2023-09-02 15:42:41 -0600

    Add stock user icon image

--- src/components/UserUnit.jsx
+++ src/components/UserUnit.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import OtherProfile from "../pages/OtherProfile";
 import AddFriend from "./AddFriend";
 import decodeEscapedData from "../utility/escape";
+import icon from "../images/accountIcon.svg"
 
 const UserUnit = ({ userObject, profile, setList }) => {
   // need to add in addFriend component
@@ -10,7 +12,7 @@ const UserUnit = ({ userObject, profile, setList }) => {
       <div className="userUnit-container">
         <Link to={`/user/${profile._id}`} element={<OtherProfile />} >
           <div className="userUnit-content">
-            <img src={profile.picture} alt="User Icon" height={'80px'}/>
+            <img src={profile.picture ? profile.picture : icon} alt="User Icon" height={'80px'}/>
             <h4>{decodeEscapedData(profile.name)}</h4>
           </div>
         </Link>

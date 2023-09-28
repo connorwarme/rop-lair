1247d136deaaa4ff75b07e52e45b7df56d4565c1
connorwarme <connor.warme@gmail.com>
2023-09-28 14:25:34 -0600

    Finish component for recognizing click elsewhere

--- src/hooks/useClickOutside.jsx
+++ src/hooks/useClickOutside.jsx
@@ -1,12 +1,10 @@
 import { useEffect } from "react"
-// left off here - working on a component to recognize if a click occurs outside of a component (ie to close a dropdown menu, etc)
-// this may work now that I debugged things... have a simple version in the Nav component
+// recognize if a click occurs outside of a component (ie to close a dropdown menu, etc)
 
 const useClickOutside = (ref, onClickOutside) => {
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (!ref.current.contains(event.target)) {
-        console.log('click outside')
         onClickOutside()
       }
       

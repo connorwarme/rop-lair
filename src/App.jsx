2b941a8124065808d3adeb8765b49febef1bd4ea
connorwarme <connor.warme@gmail.com>
2023-09-28 16:45:40 -0600

    Remove old code, format

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-// import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from "react-router-dom"
 import Nav from './components/Nav'
 import Home from './pages/Home'
-import Profile from './pages/Profile'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import Users from './pages/Users'
@@ -12,10 +10,8 @@ import Temp from './components/Temp'
 import { useState, useContext } from 'react'
 import { myContext } from './contexts/Context'
 import CreatePost from './pages/CreatePost'
-import AddFriend from './components/AddFriend'
 import UserProfile from './pages/UserProfile'
 import OtherProfile from './pages/OtherProfile'
-import Mock from './components/Mock'
 
 export default function App() {
 
@@ -26,39 +22,22 @@ export default function App() {
   // how to send auth header when needed?
   // how to refresh token when needed?
 
-  const [ list, setList ] = useState({
-    list: ['6495da6d5dea80fc65a0a443', '6495da6d5dea80fc65a0a449'],
-    pending: ['6495da6d5dea80fc65a0a443', '6495da6d5dea80fc65a0a446'],
-    request: ['6495da6d5dea80fc65a0a447'],
-  })
-  let newlist = {
-    list: ['6495da6d5dea80fc65a0a443', '6495da6d5dea80fc65a0a449'],
-    pending: ['6495da6d5dea80fc65a0a443'],
-    request: ['6495da6d5dea80fc65a0a445', '6495da6d5dea80fc65a0a447'],
-  }
-  const setNew = (listObject) => {
-    const newObj = {...userObject}
-    newObj.friend_list = listObject
-    setUserObject(newObj)
-  }
-
   return (
     <>
-        <Nav />
-        <div className="content">
-          <Routes>
-            <Route path='/' element={ <Home /> } />
-            <Route path='/auth/success' element={ <Temp /> } />
-            <Route path='/login' element={ <Login /> } />
-            <Route path='/signup' element={ <SignUp /> } />
-            <Route path='/users' element={ <Users />} />
-            <Route path='/profile' element={ <UserProfile /> } />
-            <Route path='/profile/:id' element={ <OtherProfile /> } />
-            <Route path='/post/create' element={ <CreatePost /> } />
-            <Route path='/post/:id' element={ <Post /> } />
-            <Route path='/mock' element={ <Mock /> } />
-          </Routes>
-        </div>
+      <Nav />
+      <div className="content">
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/auth/success' element={ <Temp /> } />
+          <Route path='/login' element={ <Login /> } />
+          <Route path='/signup' element={ <SignUp /> } />
+          <Route path='/users' element={ <Users />} />
+          <Route path='/profile' element={ <UserProfile /> } />
+          <Route path='/profile/:id' element={ <OtherProfile /> } />
+          <Route path='/post/create' element={ <CreatePost /> } />
+          <Route path='/post/:id' element={ <Post /> } />
+        </Routes>
+      </div>
     </>
   )
 }

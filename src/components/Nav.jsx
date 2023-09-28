3ddcbde8a9874e67dd77252d2ffa428e5a825846
connorwarme <connor.warme@gmail.com>
2023-09-27 18:02:24 -0600

    Continuing to work on nav styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import { Link } from "react-router-dom"
 import axios from "axios";
 import { clearStorage } from "../utility/ls";
@@ -10,8 +10,20 @@ import "../styles/navStyle.css"
 // want to think through how to hide routes when the visitor has not signed in yet
 // want to show routes once signed in ... but hide the sign in and login links
 const Nav = () => {
+  const [showDropdown, setShowDropdown] = useState(false)
+  const [dropdownClass, setDropdownClass] = useState('hide')
 
   const { userObject, userPhoto, makeHeader } = useContext(myContext)
+
+  const handleClickOutside = (event) => {
+    const ref = useRef(null)
+  }
+  
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true)
+
+    return document.removeEventListener('click', handleClickOutside, true)
+  }, [])
 
   // need to send auth bearer token and refresh token in request
   // then need to remove tokens from local storage
@@ -29,6 +41,17 @@ const Nav = () => {
         console.log(err)
       })
   }
+  const handleDropdown = () => {
+    setShowDropdown(!showDropdown)
+    setDropdownClass(updateClass())
+  }
+  const updateClass = () => {
+    if (showDropdown) {
+      return 'show'
+    } else {
+      return 'hide'
+    }
+  }
 
   return ( 
     <>
@@ -36,19 +59,22 @@ const Nav = () => {
         { userObject &&
         <div className="nav-links-full">
           <Link to="/" className="fan-lair">Fan Lair</Link>
-          <Link to="/post/create" className="create-post">+</Link>
+          <div className="nav-spacer"></div>
+          <Link to="/post/create" className="create-post radius"> </Link>
           <Link to="/profile" className="user">
-            { userPhoto && <img src={userPhoto} height={'30px'} /> }
-            {decodeEscapedData(userObject.name)}
+            { userPhoto && <img src={userPhoto} /> }
+            {/* {decodeEscapedData(userObject.name)} */}
           </Link>
           <div className="nav-dropdown">
-            <button type="button">
+            <button type="button" onClick={handleDropdown}>
               <img src={settingsIcon} alt="Options" />
             </button>
-            <div className="nav-dropdown-content">
+            <div className={`nav-dropdown-content ${dropdownClass}`}>
               <Link to="/users" className="users">Users</Link>
+              <div className="divider"></div>
               <Link to="/profile" className="user">My Profile</Link>
-              <li className="logout" onClick={logout}>Logout</li>
+              <div className="divider"></div>
+              <a className="logout" onClick={logout}>Logout</a>
             </div>
           </div>
         </div>

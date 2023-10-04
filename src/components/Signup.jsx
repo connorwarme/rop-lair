735c82634092ad15326e36af338b31311280f084
connorwarme <connor.warme@gmail.com>
2023-10-04 13:45:41 -0600

    Use state to update display vs directly in dom

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -8,10 +8,15 @@ import "../styles/signupStyle.css"
 
 const SignUp = ({ cancelFn }) => {
   const [firstName, setFirstName] = useState('')
+  const [first_nameErr, setFirst_NameErr] = useState(null)
   const [familyName, setFamilyName] = useState('')
+  const [family_nameErr, setFamily_NameErr] = useState(null)
   const [email, setEmail] = useState('')
+  const [emailErr, setEmailErr] = useState(null)
   const [password, setPassword] = useState('')
+  const [passwordErr, setPasswordErr] = useState(null)
   const [confirmPW, setConfPW] = useState('')
+  const [confirmPWErr, setConfPWErr] = useState(null)
   const [errors, setErrors] = useState(null)
 
   const { setAccess } = useContext(myContext)
@@ -42,10 +47,43 @@ const SignUp = ({ cancelFn }) => {
         navigate(`/`)
       } else if (res.status === 200 && res.data.errors) {
         setErrors(res.data.errors)
+        handleErrors(res.data.errors)
       }
     })
     .catch(err => {
       console.log(err)
+    })
+  }
+  const handleField = (event, setState, setError, length) => {
+    setState(event.target.value)
+    if (event.target.classList && event.target.value.length > length) {
+      setError(null)
+    }
+  }
+  const handleConfirm = (event) => {
+    setConfPW(event.target.value)
+    if (confirmPW != password) {
+      setConfPWErr(true)
+    }
+    if (event.target.value === password) {
+      setConfPWErr(null)
+    }
+  }
+  const handleErrors = (array) => {
+    array.forEach(index => {
+      if (index.path) {
+        if (index.path == 'first_name') {
+          setFirst_NameErr(true)
+        } else if (index.path == 'family_name') {
+          setFamily_NameErr(true)
+        } else if (index.path == 'email') {
+          setEmailErr(true)
+        } else if (index.path == 'password') {
+          setPasswordErr(true)
+        } else if (index.path == 'confirm_password') {
+          setConfPWErr(true)
+        }
+      }
     })
   }
 
@@ -55,23 +93,24 @@ const SignUp = ({ cancelFn }) => {
         <form action="" className="signup-form">
           <div className="form-group">
             <label htmlFor="first_name">First Name:</label>
-            <input type="text" id="first_name" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+            <input type="text" id="first_name" className={first_nameErr ? 'input-error' : null} value={firstName} onChange={(e) => handleField(e, setFirstName, setFirst_NameErr, 0)} required />
           </div>
           <div className="form-group">
             <label htmlFor="family_name">Family Name:</label>
-            <input type="text" id="family_name" value={familyName} onChange={(e) => setFamilyName(e.target.value)} required />
+            <input type="text" id="family_name" className={family_nameErr ? 'input-error' : null} value={familyName} onChange={(e) => handleField(e, setFamilyName, setFamily_NameErr, 0)} required />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <input type="email" id="email" className={emailErr ? 'input-error' : null} value={email} onChange={(e) => handleField(e, setEmail, setEmailErr, 0)} required />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <input type="password" id="password" className={passwordErr ? 'input-error' : null} value={password} onChange={(e) => handleField(e, setPassword, setPasswordErr, 6)} required />
           </div>
           <div className="form-group">
             <label htmlFor="pwd-conf">Confirm Password:</label>
-            <input type="password" id="pwd-conf" value={confirmPW} onChange={(e) => setConfPW(e.target.value)} required />
+            <input type="password" id="pwd-conf" className={confirmPWErr ? 'input-error' : null} value={confirmPW} onChange={(e) => handleConfirm(e)} required />
+            { confirmPWErr && <span className="conf-pw-error">Passwords do not match.</span> }
           </div>
           { errors && (
             <div className="errors">
@@ -79,7 +118,7 @@ const SignUp = ({ cancelFn }) => {
                 if (err.status) {
                   return <div key={index}><img src={errorIcon}/><p>{err.status} Error! {err.msg}</p></div>
                 }
-                return <div key={index}><img src={errorIcon}/><p>Error! {err.msg}</p></div>
+                return <div key={index}><img src={errorIcon}/><p>{err.msg}</p></div>
               })}
             </div>
           )}          

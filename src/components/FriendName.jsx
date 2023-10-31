c2d39eed2abfc370dc7f0218e4848da14315ebee
connorwarme <connor.warme@gmail.com>
2023-10-30 19:47:07 -0600

    Continue styling, add prof pic on friend list

--- src/components/FriendName.jsx
+++ src/components/FriendName.jsx
@@ -1,6 +1,7 @@
 import useAxios from "../hooks/useAxios";
 import { Link } from "react-router-dom";
 import decodeEscapedData from "../utility/escape";
+import icon from "../images/icons/account.svg"
 import errorIcon from "../images/icons/error.svg"
 
 const FriendName = ({ userid, makeHeader, setLoading }) => {
@@ -21,7 +22,11 @@ const FriendName = ({ userid, makeHeader, setLoading }) => {
         <>
           { data.user && (
             <>
-              <Link to={`/profile/${data.user._id}`} onClick={handleClick}>{decodeEscapedData(data.user.name)}</Link>
+              <Link to={`/profile/${data.user._id}`} onClick={handleClick} className="friend-name-link">
+                <img src={data.photoPath ? data.photoPath : icon} />
+                { data && console.log(data.photoPath) }
+                <p>{decodeEscapedData(data.user.name)}</p>
+              </Link>
             </>
           )}
           { data.errors && <div className="friend-error-container"><img src={errorIcon}/><p>{data.errors[0].msg}</p></div> }

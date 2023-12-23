b558d5651e8db20a9aebed6dc1fd0897fcf741b0
connorwarme <connor.warme@gmail.com>
2023-12-22 20:21:59 -0700

    Implement new userPhoto comp

--- src/components/UserUnit.jsx
+++ src/components/UserUnit.jsx
@@ -1,18 +1,17 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
+import UserPhoto from "./UserPhoto";
 import AddFriend from "./AddFriend";
 import decodeEscapedData from "../utility/escape";
-import icon from "../images/icons/accountIcon.svg"
 
-const UserUnit = ({ userObject, profile, photo, setList }) => {
-  
+const UserUnit = ({ userObject, profile, setList, makeHeader }) => {
+
   return ( 
     <>
       <div className="userUnit-container">
         <Link to={`/profile/${profile._id}`}  >
           <div className="userUnit-content">
-            { photo && <img src={photo} alt="Icon" /> }
-            { !photo && <img src={icon} alt="Icon" className="account-icon" /> }
+            <UserPhoto userId={profile._id} makeHeader={makeHeader} />
             <h4>{`${decodeEscapedData(profile.first_name)} ${decodeEscapedData(profile.family_name)}`}</h4>
           </div>
         </Link>
